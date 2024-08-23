000418a5783c595008df677dcdb663bd31107b28
Breck Echelberger <breckech@gmail.com>
2024-08-23 12:17:29 -0500

    first

---
+++ day_three_inclass.py
@@ -0,0 +1,40 @@
+print("Hello there.", end = "\n\n")
+print("I'm Breck.")
+
+print("Hello there.", end = " ")
+print("I'm Breck.")
+
+wage = 20
+print(wage) # print the variables value.
+
+print("1\n2\n3\n") # creating new lines. these are called white characters.
+print("1\t")
+
+# reading is achieved using the input function.
+print("Enter the name of your bestfriend:", end = " ") # the end keeps the input on the same line.
+best_friend = input()
+print("My best friend is", best_friend)
+
+# strings and integers are example of a type of data
+my_string = "123"
+my_int = int(123)
+print(my_string, my_int)
+
+int(1)
+
+# natural numbers is the smallest number set and complex numbers is the biggest set.
+
+print(float(3.2))
+# if a regular int is used it will cut off everything after the decimal>
+
+# this is called a pound, according to the prof.
+# it is a command and in python it will ignore everything after the pound. It is meant for the reader.
+
+hours = 10
+hourly_wage = int(input("Enter hourly wage: "))
+print("Salary is", hourly_wage * hours)
+
+# operator for multiplication = *, operator for exponents = **
+
+name = input("What is your first, middle and last name?: ")
+print("Howdy", name+"!") # the + gets rid of the space. Whenever a comma is used there will be a space.
